255934a5b4eb343e4340029f2bf38adbcd7ab5e0
wmcaeddy <wmcaeddy@yahoo.com.hk>
2026-01-13 14:33:52 +0800

    style: remove language selection dropdown from header

--- components/header.php
+++ components/header.php
@@ -16,19 +16,11 @@
                             <ul class="navbar-nav ml-auto header-v1__menu-list"></ul>
                         </div>
                         <ul class="header-v1__search">
-                            <li class="dropdown langOption ml-2 mr-2">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            <li class="langOption ml-2 mr-2">
+                                <a href="#" role="button">
                                     <img alt="TW - contry flag icon" src="assets/theme/tw.svg" style="display: inline-block; width: 1em; height: 1em; vertical-align: middle;">
                                     <span class="text"> 繁體中文</span>
-                                    <i class="icon"><img src="assets/theme/dwn-arw-grey.svg"></i>
                                 </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item " role="button"><img alt="GB - contry flag icon" src="assets/theme/gb.svg" style="display: inline-block; width: 1em; height: 1em; vertical-align: middle;"> English</a>
-                                    <a class="dropdown-item active" role="button"><img alt="TW - contry flag icon" src="assets/theme/tw.svg" style="display: inline-block; width: 1em; height: 1em; vertical-align: middle;"> 繁體中文</a>
-                                    <a class="dropdown-item " role="button"><img alt="CN - contry flag icon" src="assets/theme/cn.svg" style="display: inline-block; width: 1em; height: 1em; vertical-align: middle;"> 简体中文</a>
-                                    <a class="dropdown-item " role="button"><img alt="JP - contry flag icon" src="assets/theme/jp.svg" style="display: inline-block; width: 1em; height: 1em; vertical-align: middle;"> 日本語</a>
-                                    <a class="dropdown-item " role="button"><img alt="DE - contry flag icon" src="assets/theme/de.svg" style="display: inline-block; width: 1em; height: 1em; vertical-align: middle;"> Deutsch</a>
-                                </div>
                             </li>
                         </ul>
                         <div class="header-v1__utils"><div class="header-v1__user"></div></div>
